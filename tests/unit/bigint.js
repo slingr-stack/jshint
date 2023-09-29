da15cb684dbb470c794162ba4c620c76a86f8bb3
JuanEspeche <jespeche@slingr.io>
2023-09-29 11:48:54 -0300

    init

---
+++ tests/unit/bigint.js
@@ -0,0 +1,104 @@
+"use strict";
+
+var TestRun = require("../helpers/testhelper").setup.testRun;
+
+exports.enabling = function (test) {
+  TestRun(test, "Not enabled")
+    .addError(1, 6, "'BigInt' is only available in ES11 (use 'esversion: 11').")
+    .test("void 1n;", {esversion: 10});
+
+  TestRun(test, "Enabled via inline directive")
+    .test([
+      "// jshint esversion: 11",
+      "void 1n;"
+    ], {esversion: 10});
+
+  TestRun(test, "Enabled via configuration object")
+    .test([
+      "void 1n;"
+    ], {esversion: 11});
+
+  test.done();
+};
+
+exports.validUsage = function(test) {
+  TestRun(test)
+    .test([
+      "void 0n;",
+      "void 9n;",
+      "void 0x0n;",
+      "void 0xfn;",
+      "void 0o0n;",
+      "void 0o7n;",
+      "void 0b0n;",
+      "void 0b1n;",
+      "void 0b01n;",
+      "void 0b10n;",
+    ], {esversion: 11});
+
+  TestRun(test, 'No warnings for values that would otherwise coerce to Infinity')
+    .test([
+      "void " +
+        "1000000000000000000000000000000000000000000000000000000000000000000" +
+        "0000000000000000000000000000000000000000000000000000000000000000000" +
+        "0000000000000000000000000000000000000000000000000000000000000000000" +
+        "0000000000000000000000000000000000000000000000000000000000000000000" +
+        "000000000000000000000000000000000000000000n;"
+    ], {esversion: 11});
+
+  test.done();
+};
+
+exports.invalid = function (test) {
+  TestRun(test, "preceding decimal point")
+    .addError(1, 10, "A leading decimal point can be confused with a dot: '.1'.")
+    .addError(1, 8, "Missing semicolon.")
+    .addError(1, 8, "Expected an assignment or function call and instead saw an expression.")
+    .test("void 1n.1;", {esversion: 11});
+
+  TestRun(test, "following decimal point")
+    .addError(1, 6, "Unexpected '1'.")
+    .addError(1, 1, "Unexpected early end of program.")
+    .addError(1, 6, "Unrecoverable syntax error. (100% scanned).")
+    .test("void 1.1n;", {esversion: 11});
+
+  TestRun(test, "preceding exponent")
+    .addError(1, 6, "Unexpected '1'.")
+    .addError(1, 1, "Unexpected early end of program.")
+    .addError(1, 6, "Unrecoverable syntax error. (100% scanned).")
+    .test("void 1ne3;", {esversion: 11});
+
+  TestRun(test, "following exponent")
+    .addError(1, 6, "Unexpected '1'.")
+    .addError(1, 1, "Unexpected early end of program.")
+    .addError(1, 6, "Unrecoverable syntax error. (100% scanned).")
+    .test("void 1e3n;", {esversion: 11});
+
+  TestRun(test, "invalid legacy octal")
+    .addError(1, 6, "Malformed numeric literal: '01n'.")
+    .test("void 01n;", {esversion: 11});
+
+  TestRun(test, "invalid leading 0")
+    .addError(1, 6, "Malformed numeric literal: '08n'.")
+    .test("void 08n;", {esversion: 11});
+
+  TestRun(test, "invalid hex digit")
+    .addError(1, 8, "Malformed numeric literal: '0x'.")
+    .addError(1, 8, "Missing semicolon.")
+    .addError(1, 8, "Expected an assignment or function call and instead saw an expression.")
+    .test("void 0xgn;", {esversion: 11});
+
+  TestRun(test, "invalid binary digit")
+    .addError(1, 8, "Malformed numeric literal: '0b'.")
+    .addError(1, 8, "Missing semicolon.")
+    .addError(1, 8, "Expected an assignment or function call and instead saw an expression.")
+    .test("void 0b2n;", {esversion: 11});
+
+  TestRun(test, "invalid octal digit")
+    .addError(1, 8, "Malformed numeric literal: '0o'.")
+    .addError(1, 8, "Missing semicolon.")
+    .addError(1, 8, "Expected an assignment or function call and instead saw an expression.")
+    .test("void 0o8n;", {esversion: 11});
+
+  test.done();
+};
---
+++ tests/unit/fixtures/es6-import-export.js
@@ -0,0 +1,77 @@
+/* jshint undef:true */
+
+import $ from "jquery";
+import { get as emGet, set } from "ember";
+import { one } from "foo";
+import { default as _ } from "underscore";
+import {} from "ember";
+import "ember";
+import * as ember2 from "ember";
+import _2, * as ember3 from "ember";
+import _3, { default as ember } from "ember";
+
+$.ajax();
+emGet("foo");
+set("bar");
+_.map();
+
+var foo = "foo";
+var bar = "bar";
+function foobar() {}
+
+export default foobar;
+
+
+
+
+
+
+
+export { foo };
+export { foo, bar } from "source";
+export { foo, bar as biz } from "source";
+
+// gettin' fancy
+
+export function a() {
+  return "a";
+}
+
+export var b = function() {
+  return "b";
+};
+
+export var c = "c";
+
+export class Foo {}
+export class List extends Array {}
+
+
+// imports are const's and cannot be re-assigned
+$ = null;
+emGet = null;
+one = null;
+_ = null;
+ember2 = null;
+// they also cannot be imported twice
+import $ from "jquery";
+import { get as emGet, set } from "ember";
+import { default as _ } from "underscore";
+import * as ember2 from "ember";
+// or used before definition
+if (newImport) {
+  $();
+}
+import newImport from 'newImport';
+
+export function* gen() { yield 1; }
+
+// Trailing comma
+var w;
+export { w, };
+import { x, } from "source";
+
+// Empty
+export {};
+export {} from "source";
+import {} from "source";
